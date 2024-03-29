bd56a5ea5e0f7ef50c899a605d8861e9a5ad55d4
aarchie23 <aarchiesharma10@gmail.com>
2024-03-29 04:33:10 -0700

    pull changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,30 +1,43 @@
-const userService=require("../services/user.service")
+const userService = require("../services/user.service");
 
-const getUserProfile=async (req,res)=>{
+const getUserProfile = async (req, res) => {
     try {
-        const jwt= req.headers.authorization?.split(' ')[1];
+        const jwt = req.headers.authorization?.split(' ')[1];
 
-        if(!jwt){
-            return res.status(404).send({error:"token not found"})
+        if (!jwt) {
+            return res.status(404).send({ error: "Token not found" });
         }
-        const user=await userService.getUserProfileByToken(jwt)
+        const user = await userService.getUserProfileByToken(jwt);
 
-        return res.status(200).send(user)
+        return res.status(200).send(user);
 
-    
     } catch (error) {
-        console.log("error from controller - ",error)
-        return res.status(500).send({error:error.message})
+        console.log("Error from controller - ", error);
+        return res.status(500).send({ error: error.message });
     }
-}
+};
 
-const getAllUsers=async(req,res)=>{
+const getAllUsers = async (req, res) => {
     try {
-        const users=await userService.getAllUsers()
-        return res.status(200).send(users)
+        const users = await userService.getAllUsers();
+        return res.status(200).send(users);
     } catch (error) {
-        return res.status(500).send({error:error.message})
+        return res.status(500).send({ error: error.message });
     }
-}
+};
 
-module.exports={getUserProfile,getAllUsers}
+const updateUserPersonalInfo = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const updatedInfo = req.body;
+
+        const updatedUser = await updateUserPersonalInfo(userId, updatedInfo);
+        return res.status(200).send(updatedUser);
+    } catch (error) {
+        console.log("Error from controller - ", error);
+        return res.status(500).send({ error: error.message });
+    }
+};
+
+
+module.exports = { getUserProfile, getAllUsers, updateUserPersonalInfo };
